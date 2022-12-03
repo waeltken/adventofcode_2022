d60ba780ef9b70138fcc904b2cb80547bf8d7c64
Clemens Wältken <clemens.waltken@microsoft.com>
2022-12-03 12:27:37 +0100

    Day3 part 2

--- day3_backpacks/main.py
+++ day3_backpacks/main.py
@@ -1,4 +1,5 @@
 from typing import Tuple
+from numpy import reshape
 
 sample_strings = [
     "vJrwpWtwJgWrhcsFMMfFFhFp",
@@ -38,6 +39,18 @@ def read_lines_from_file():
         return f.readlines()
 
 
+def group_elves_backpacks(backpacks: list) -> list:
+    return reshape(backpacks, (-1, 3))
+
+
+def find_label_item(backpacks: list) -> chr:
+    all_items = "".join(backpacks)
+    for item in all_items:
+        (a, b, c) = backpacks
+        if item in a and item in b and item in c:
+            return item
+
+
 def main():
     backpacks = [split_backpack(backpack) for backpack in sample_strings]
     backpacks = [split_backpack(backpack) for backpack in read_lines_from_file()]
@@ -50,6 +63,13 @@ def main():
         item_priority(find_duplicate(backpack)) for backpack in backpacks
     ]
     print(f"Sum of item priorities: {sum(item_priorities)}")
+
+    groups = group_elves_backpacks(sample_strings)
+    groups = group_elves_backpacks(read_lines_from_file())
+
+    label_items = [find_label_item(group) for group in groups]
+    label_item_priorities = [item_priority(item) for item in label_items]
+    print(f"Sum of label item priorities: {sum(label_item_priorities)}")
 
 
 if __name__ == "__main__":
