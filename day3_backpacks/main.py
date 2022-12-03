221b7d6352fb788c3210313b6d1b7a02004b49a1
Clemens Wältken <clemens.waltken@microsoft.com>
2022-12-03 11:58:45 +0100

    Day3 part 1

---
+++ day3_backpacks/main.py
@@ -0,0 +1,56 @@
+from typing import Tuple
+
+sample_strings = [
+    "vJrwpWtwJgWrhcsFMMfFFhFp",
+    "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+    "PmmdzqPrVvPwwTWBwg",
+    "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+    "ttgJtRGJQctTZtZT",
+    "CrZsJsPPZsGzwwsLwLmpwMDw",
+]
+
+Backpack = Tuple[str, str]
+
+
+def split_backpack(backpack: str) -> Backpack:
+    return backpack[: len(backpack) // 2], backpack[len(backpack) // 2 :]
+
+
+def find_duplicate(backpack: Backpack) -> chr:
+    (left, right) = backpack
+    for letter in left:
+        if letter in right:
+            return letter
+
+
+def item_priority(chr) -> int:
+    return ord(chr) - 96 if chr.islower() else ord(chr) - 38
+
+
+assert item_priority("a") == 1
+assert item_priority("z") == 26
+assert item_priority("L") == 38
+assert item_priority("P") == 42
+
+
+def read_lines_from_file():
+    with open("./day3_backpacks/input.txt", "r") as f:
+        return f.readlines()
+
+
+def main():
+    backpacks = [split_backpack(backpack) for backpack in sample_strings]
+    backpacks = [split_backpack(backpack) for backpack in read_lines_from_file()]
+    for backpack in backpacks:
+        duplicate = find_duplicate(backpack)
+        print(
+            f"Backpack: {backpack}, Duplicate letter: ({duplicate}, {item_priority(duplicate)})",
+        )
+    item_priorities = [
+        item_priority(find_duplicate(backpack)) for backpack in backpacks
+    ]
+    print(f"Sum of item priorities: {sum(item_priorities)}")
+
+
+if __name__ == "__main__":
+    main()
