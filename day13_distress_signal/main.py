773b474347291814d64167ac7665d63d8826129b
Clemens Wältken <clemens.waltken@microsoft.com>
2022-12-13 12:37:27 +0100

    First implementation, error in large input

--- day13_distress_signal/main.py
+++ day13_distress_signal/main.py
@@ -2,3 +2,36 @@ class IntcodeComputer:
     def __init__(self, input):
         self.input = input
         self.pairs = [pair.split("\n") for pair in input.split("\n\n")]
+        self.pairs = [[eval(left), eval(right)] for left, right in self.pairs]
+
+    def pairs_in_order(self):
+        return [is_in_order(pair) for pair in self.pairs]
+
+    def sum_true_indices(self):
+        return sum_true_indices(self.pairs_in_order())
+
+
+def is_in_order(pair):
+    left_list, right_list = pair
+    if not left_list:
+        return True
+    if not right_list:
+        return False
+    left, *left_tail = left_list
+    right, *right_tail = right_list
+    if isinstance(left, int) and isinstance(right, int):
+        if left > right:
+            return False
+        return is_in_order([left_tail, right_tail])
+    # check if left and right are lists
+    if isinstance(left, list) and isinstance(right, list):
+        return is_in_order([left, right])
+    # check if left is a list and right is an int
+    if isinstance(left, list) and isinstance(right, int):
+        return is_in_order([[left, *left_tail], [[right], *right_tail]])
+    if isinstance(left, int) and isinstance(right, list):
+        return is_in_order([[[left], *left_tail], [right, *right_tail]])
+
+
+def sum_true_indices(pairs_in_order):
+    return sum([index + 1 for index, value in enumerate(pairs_in_order) if value])
--- day13_distress_signal/test.py
+++ day13_distress_signal/test.py
@@ -27,10 +27,26 @@ sample_input = """[1,1,3,1,1]
 [1,[2,[3,[4,[5,6,0]]]],8,9]"""
 
 
+def read_input():
+    with open("day13_distress_signal/input.txt") as f:
+        return f.read().strip()
+
+
 class TestDay13(unittest.TestCase):
-    def test_part1(self):
+    def test_sample(self):
         computer = IntcodeComputer(sample_input)
+        pairs_in_order = computer.pairs_in_order()
         self.assertEqual(len(computer.pairs), 8)
+        self.assertEqual(
+            pairs_in_order,
+            [True, True, False, True, False, True, False, False],
+        )
+        self.assertEqual(computer.sum_true_indices(), 13)
+
+    def test_input(self):
+        computer = IntcodeComputer(read_input())
+        self.assertEqual(len(computer.pairs), 150)
+        self.assertEqual(computer.sum_true_indices(), 4010)
 
 
 if __name__ == "__main__":
