63687f58b1af18863da14de00620d934c38e3e19
Clemens Wältken <clemens.waltken@microsoft.com>
2022-12-13 15:10:33 +0100

    Refactor part 1

--- day13_distress_signal/main.py
+++ day13_distress_signal/main.py
@@ -5,38 +5,37 @@ class IntcodeComputer:
         self.pairs = [[eval(left), eval(right)] for left, right in self.pairs]
 
     def pairs_in_order(self):
-        return [is_in_order(pair) for pair in self.pairs]
+        return [compare(left, right) < 0 for (left, right) in self.pairs]
 
     def sum_true_indices(self):
         return sum_true_indices(self.pairs_in_order())
 
 
-def is_in_order(pair):
-    left_list, right_list = pair
+def compare(left_list, right_list):
     if not left_list and right_list:
-        return True
+        return -1
     if not right_list and left_list:
-        return False
+        return 1
     if not left_list and not right_list:
-        return None
+        return 0
     left, *left_tail = left_list
     right, *right_tail = right_list
     if isinstance(left, int) and isinstance(right, int):
         if left < right:
-            return True
+            return -1
         elif left > right:
-            return False
-        return is_in_order([left_tail, right_tail])
+            return 1
+        return compare(left_tail, right_tail)
     # check if left and right are lists
     if isinstance(left, list) and isinstance(right, list):
-        if is_in_order([left, right]) is None:
-            return is_in_order([left_tail, right_tail])
-        return is_in_order([left, right])
+        if compare(left, right) is 0:
+            return compare(left_tail, right_tail)
+        return compare(left, right)
     # check if left is a list and right is an int
     if isinstance(left, list) and isinstance(right, int):
-        return is_in_order([[left, *left_tail], [[right], *right_tail]])
+        return compare([left, *left_tail], [[right], *right_tail])
     if isinstance(left, int) and isinstance(right, list):
-        return is_in_order([[[left], *left_tail], [right, *right_tail]])
+        return compare([[left], *left_tail], [right, *right_tail])
 
 
 def sum_true_indices(pairs_in_order):
